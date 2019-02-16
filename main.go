2abc16fbe418ea7fcf225d3e825dd5356e0dcccb
Adron Hall <adron@thrashingcode.com>
2019-02-16 10:58:27 -0800

    Added additional variadic function example.

--- main.go
+++ main.go
@@ -4,12 +4,28 @@ import "fmt"
 
 func main() {
 	fmt.Printf("Sum: %d\n", sum(42, 42, 42, 96, 102, 3))
+
+	theNumbers := []int{42, 51, 23, 62, 102, 9, 23, 64}
+
+	sum, sentence := sumAndPresent(theNumbers,
+		"Hello, ", "cheeseheads", " of the world!")
+
+	fmt.Printf("Sum: %d\nSentence: %s", sum, sentence)
 }
 
 func sum(vals ...int) int {
 	total := 0
-	for _, val :=  range vals {
+	for _, val := range vals {
 		total += val
 	}
 	return total
+}
+
+func sumAndPresent(vals []int, words ...string) (int, string) {
+	total := sum(vals...)
+	sentences := "To say: "
+	for _, word := range words {
+		sentences += word
+	}
+	return total, sentences
 }
